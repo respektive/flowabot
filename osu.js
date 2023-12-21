3779d1661cbda0fe1e305e637ac5c7ac7caa4a7d
respektive <respektivee@gmail.com>
2023-12-21 16:27:14 +0100

    handle custom dt rates in with

--- osu.js
+++ osu.js
@@ -1884,6 +1884,21 @@ module.exports = {
             //helper.log(response);
 
             const beatmap = response.beatmap;
+
+            if(options.speed_change && options.speed_change > 1) {
+                let mod = options.mods.find(m => m.acronym === "DT")
+                if(mod)
+                    mod.settings = { speed_change: options.speed_change};
+                else 
+                    options.mods.push({acronym: "DT", settings: {speed_change: options.speed_change}})
+            } else if(options.speed_change && options.speed_change < 1) {
+                let mod = options.mods.find(m => m.acronym === "HT")
+                if(mod)
+                    mod.settings = { speed_change: options.speed_change};
+                else 
+                    options.mods.push({acronym: "HT", settings: {speed_change: options.speed_change}})
+            }
+
             let mods = options.mods.map(mod => mod.acronym)
             if(!mods)
                 mods = [];
@@ -1957,7 +1972,7 @@ module.exports = {
             embed.color = 12277111;
             embed.title = `${beatmap.artist} – ${beatmap.title} [${beatmap.version}]`;
             embed.url = `https://osu.ppy.sh/b/${beatmap.beatmap_id}`;
-            embed.description = `**${mods.length > 0 ? '+' + mods.join('') : 'NOMOD'}**`;
+            embed.description = `**${mods.length > 0 ? '+' + sanitizeMods(options.mods).join('') : 'NOMOD'}**`;
 
             let lines = ['', '', 'Difficulty', ''];
 
